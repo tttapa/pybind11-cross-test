f99efe8317c3d1d897d45eb56e20ae02af267b51
Pieter Pas <pieter.p.dev@outlook.com>
2024-04-01 15:14:13 +0200

    Use CMAKE_TOOLCHAIN_FILE instead of --toolchain

--- build-pybind11.sh
+++ build-pybind11.sh
@@ -4,7 +4,7 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 set -ex
 
 cmake -S pybind11 -B build/pybind11 \
-    --toolchain="$PWD/toolchain.cmake" \
+    -D CMAKE_TOOLCHAIN_FILE="$PWD/toolchain.cmake" \
     -D BUILD_TESTING=Off -D PYBIND11_NOPYTHON=On \
     -D CMAKE_INSTALL_PREFIX="$PWD/staging/pybind11/usr/local"
 cmake --build build/pybind11
